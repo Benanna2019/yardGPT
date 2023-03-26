98a86eb1536df81223cb8875435fae17970fd6cd
Benanna2019 <bass41992ben@gmail.com>
2023-03-26 17:33:54 -0400

    removed unnecessary next config stuff and files

--- next.config.js
+++ next.config.js
@@ -4,18 +4,4 @@ module.exports = {
   images: {
     domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com", 'icons8.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/github',
-        destination: 'https://github.com/Nutlope/roomGPT',
-        permanent: false,
-      },
-      {
-        source: '/deploy',
-        destination: 'https://vercel.com/templates/next.js/room-GPT',
-        permanent: false,
-      },
-    ]
-  },
 }
